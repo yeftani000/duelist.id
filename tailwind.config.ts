e4b6248c00521dfaa8d0ae777b4f4cf70da43580
yeftani000 <37604131+yeftani000@users.noreply.github.com>
2026-04-27 17:05:06 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,16 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        glass: "rgba(255, 255, 255, 0.03)",
+        "glass-border": "rgba(255, 255, 255, 0.1)",
+      },
+    },
   },
   plugins: [],
 };
-export default config;
