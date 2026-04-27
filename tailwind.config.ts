8cc3218ec33e3b34f0ff125ee8256ea0517764ef
yeftani000 <37604131+yeftani000@users.noreply.github.com>
2026-04-27 16:11:50 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // We are adding EVERY possible path to make sure Tailwind finds your classes
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
