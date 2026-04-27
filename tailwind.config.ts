b1da8130be63e158dae36cf34a382e6684844835
yeftani000 <37604131+yeftani000@users.noreply.github.com>
2026-04-27 16:25:44 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // We are adding EVERY possible path to make sure Tailwind finds your classes
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        glass: "rgba(255, 255, 255, 0.05)",
+        "glass-border": "rgba(255, 255, 255, 0.1)",
+      }
+    },
   },
   plugins: [],
 };
