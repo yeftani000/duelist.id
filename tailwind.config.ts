dc6a5c247a45dfff5f2d800638a36555a0db74cb
yeftani000 <37604131+yeftani000@users.noreply.github.com>
2026-04-27 16:59:54 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // This tells Tailwind: "Look inside these folders for my styling classes!"
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        glass: "rgba(255, 255, 255, 0.05)",
-        "glass-border": "rgba(255, 255, 255, 0.1)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
