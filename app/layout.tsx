17e3d1760fcfcdc55e496650f92902de0e548a0a
yeftani000 <37604131+yeftani000@users.noreply.github.com>
2026-04-27 15:39:47 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css"; // MUST BE AT THE TOP
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Duelist.id | YGO Price Aggregator",
+  description: "Find the best Yu-Gi-Oh! card prices in Indonesia",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="selection:bg-blue-500/30">
+        {children}
+      </body>
+    </html>
+  );
+}
